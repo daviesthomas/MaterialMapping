0b151b720f8c5066ab4cc63bbbc3d1a023b1e38b
Thomas Davies <daviest@Thomass-MacBook-Pro.local>
2020-03-22 09:18:12 -0400

    sample generating random renders with random materials

--- DataTools/datagen.py
+++ DataTools/datagen.py
@@ -14,24 +14,29 @@ from blender.scene import Scene
 from blender.render import Render
 
 def main():
-	renderer = Render(1024, 1024, exposure=1)
+	renderer = Render(256, 256, exposure=1, numSamples=64)
 	
 	scene = Scene()
 
 	scene.setAmbientLight((0.2,0.2,0.2,1))
 	scene.addSunLight((-15, -34, -155), 2, 0.1)
 
-	geometry = Geometry('examples/gear_knee.stl')
+	geometry = Geometry('examples/blenderSphere.stl', rotation_euler=(0,0.0,90.0))
 	#subdivide for smooth shading
 	geometry.subdivide(2)
-	
-	bsdfMaterial = PrincipledBSDF()
-	geometry.setBSDF(bsdfMaterial)
 
 	camLocation = (1.9,2,2.2)
 	lookAtLocation = (0,0,0.5)
 	cam = Camera(location=camLocation, lookat=lookAtLocation, focalLength= 45).bpyCam()
 
-	renderer.run('test.png', cam)
+	for i in range (200):
+		bsdfMaterial = PrincipledBSDF()
+		bsdfMaterial.randomize()
+
+		geometry.setBSDF(bsdfMaterial)
+
+		renderer.run('examples/renders/render-{}.png'.format(i), cam)
+		bsdfMaterial.save('examples/renders/material-{}.json'.format(i))
+
 
 main()
