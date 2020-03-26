242c460940baf297fe7a1fcec9be381f4817039f
Thomas Davies <daviest@TORPC0EW0FA.ads.autodesk.com>
2020-03-25 22:40:36 -0400

    arg parse class for overriding blender argparse

---
+++ DataTools/blender/argparser.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+#blender uses argparse... this is a simple workaround to force blender to ignore args specific to this app
+#https://blender.stackexchange.com/questions/6817/how-to-pass-command-line-arguments-to-a-blender-python-script
+class ArgumentParserForBlender(argparse.ArgumentParser):
+    def _get_argv_after_doubledash(self):
+        """
+        Given the sys.argv as a list of strings, this method returns the
+        sublist right after the '--' element (if present, otherwise returns
+        an empty list).
+        """
+        try:
+            idx = sys.argv.index("--")
+            return sys.argv[idx+1:] # the list after '--'
+        except ValueError as e: # '--' not in the list:
+            return []
+
+    # overrides superclass
+    def parse_args(self):
+        """
+        This method is expected to behave identically as in the superclass,
+        except that the sys.argv list will be pre-processed using
+        _get_argv_after_doubledash before. See the docstring of the class for
+        usage examples and details.
+        """
+        return super().parse_args(args=self._get_argv_after_doubledash())
